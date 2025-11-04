1116abc813b59d00d2ed8d37e210b7b1b10348ee
vaishnavbhosale <vaishnavbharatbhosale@gmail.com>
2025-11-04 20:26:07 +0530

    feat: added Spring Security with role-based access control (Admin/User)

---
+++ src/main/java/com/Intern/InternPlus/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.Intern.InternPlus.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("user123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/internships").permitAll() // everyone can view internships
+                        .requestMatchers("/api/internships/**").hasRole("ADMIN") // only admin can add/delete
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.permitAll())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
+
